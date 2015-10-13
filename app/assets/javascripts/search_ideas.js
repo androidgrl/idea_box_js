4554922d7f35de356bc14531ead303f61903aa58
androidgrl <kawaharajamie@gmail.com>
2015-10-13 16:56:27 -0600

    Steve helped me fix filtering
    So, I was hiding everything, and then
    showing a child element inside.  But when
    you hide something, you can only show that
    same exact thing.

--- app/assets/javascripts/search_ideas.js
+++ app/assets/javascripts/search_ideas.js
@@ -1,9 +1,21 @@
 function searchIdeas () {
-    console.log(this.value);
-    $('#ideas').children().each(function (index, idea) {
-        var $idea = $(idea);
-        console.log($idea.data('title'));
-    });
+    var dis = this.value;
+    console.log(dis);
+    if (dis.length > 0) {
+        $('#ideas').children().each(function (index, idea) {
+            var $idea = $(idea);
+            var titleAndBody = $idea.data('title') + $idea.data('body');
+            console.log(titleAndBody);
+            console.log(dis);
+            if (titleAndBody.indexOf(dis) !== -1) {
+                $idea.show();
+            } else {
+                $idea.hide();
+            }
+        });
+    } else {
+        $('#ideas').show();
+    }
 }
 
 $('document').ready(function(){
