ec02e2db1dc8bee5210b8309baf56fcaa886693c
androidgrl <kawaharajamie@gmail.com>
2015-10-12 19:58:44 -0600

    Add responders gem
    The test said view was missing, so
    I want the create method to respond with
    json object, so I added responders gem.
    
    Now the error is: "Failure/Error:
    expect(idea_response['idea']['title']).to eq('purple pancakes')
         NoMethodError:
                undefined method `[]' for nil:NilClass'`"

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,2 +1,14 @@
 class IdeasController < ApplicationController
+  respond_to :json
+
+  def create
+    @idea = Idea.create(ideas_params)
+    respond_with @idea
+  end
+
+  private
+
+  def ideas_params
+    params.require(:idea).permit(:title, :body)
+  end
 end
