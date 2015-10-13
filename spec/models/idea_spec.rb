59813cbffeaaf1d33025455371ed3444d3057811
androidgrl <kawaharajamie@gmail.com>
2015-10-12 20:19:53 -0600

    Add Idea model test

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Idea, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid" do
+    idea = Idea.create(title: "say hi", body: "to everyone")
+    expect(idea).to be_valid
+  end
 end
