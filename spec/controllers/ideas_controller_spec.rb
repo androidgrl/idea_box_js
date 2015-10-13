ecac2aa426931c07db8820c957245410250403f2
androidgrl <kawaharajamie@gmail.com>
2015-10-12 19:32:37 -0600

    Add controller test
    It says need to run rake db migrate.

---
+++ spec/controllers/ideas_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe IdeasController do
+  context "#create" do
+    it "creates an idea" do
+      post :create, format: :json, idea: { title: 'purple pancakes', body: 'make taro-based pancakes' }
+      expect(response).to have_http_status(:created)
+      idea_response = JSON.parse(response.body)
+
+      expect(idea_response['idea']['title']).to eq('purple pancakes')
+      expect(idea_response['idea']['body']).to eq('make taro-based pancakes')
+    end
+  end
+end
