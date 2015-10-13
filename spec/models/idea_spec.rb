c19a2a26b0db90582dfb22ee994ea7bb246124a3
androidgrl <kawaharajamie@gmail.com>
2015-10-12 20:23:45 -0600

    Add more model tests
    For default quality.

--- spec/models/idea_spec.rb
+++ spec/models/idea_spec.rb
@@ -5,4 +5,16 @@ RSpec.describe Idea, type: :model do
     idea = Idea.create(title: "say hi", body: "to everyone")
     expect(idea).to be_valid
   end
+
+  it "has a default quality of swill" do
+    idea = Idea.create(title: "say hi", body: "to everyone")
+    expect(idea.quality).to eq(0)
+  end
+
+  it "can change qualities" do
+    idea = Idea.create(title: "say hi", body: "to everyone")
+    idea.update_attributes(quality: 1)
+
+    expect(idea.quality).to eq(1)
+  end
 end
